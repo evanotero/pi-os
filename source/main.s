e2218e61de7cebd1b2e11c403fcfd0cf980be38a
evanotero <oteroev@bc.edu>
2016-11-28 20:30:20 -0500

    Complete OK02

--- source/main.s
+++ source/main.s
@@ -3,21 +3,40 @@
  * Author:  Evan Otero
  *
  * ARMv6
- * An operating system that turns on the OK LED
+ * An operating system that turns the OK LED on and off repeatedly.
  */
 
 .section .init              @ directive to linker to put this code first
 .globl _start               @ directive to toolchain to tell where entry point is
+
 _start:
-    ldr     r0, =0x20200000 @ address of GPIO controller
+    LDR     R0, =0x20200000 @ address of GPIO controller
 
-    mov     r1, #1          @ r1 = 1
-    lsl     r1, #18         @ 6th set of 3bits -> left shift 6 * 3 = 18 -> r1 = 1000000000000000000
-    str     r1, [r0, #4]    @ enable GPIO 16 pin for output
+    MOV     R1, #1
+    LSL     R1, #18         @ 6th set of 3 bits
+    STR     R1, [r0, #4]    @ enable GPIO 16 pin for output
 
-    mov     r1, #1          @ r1 = 1
-    mov     r1, #16         @ left shift 16 -> r1 = 10000000000000000
-    str     r1, [r0, #40]   @ set GPIO 16 to low, causing the LED to turn on
+    MOV     R1, #1
+    MOV     R1, #16         @ put a 1 in 16th bit of message
 
-loop$:
-    b       loop$           @ give Pi task to do forever, so it will not crash
+/* Turn LED on and off frepeatedly */
+LOOP$:
+    STR     R1, [r0, #40]   @ set GPIO 16 to LOW (40) -> LED turns ON
+    MOV     R2, #0x3F0000   @ initialize wait start amount
+
+/* Wait until R2 == 0 */
+WAIT1$:
+    SUB     R2, #1
+    CMP     R2, #0
+    BNE     WAIT1$
+
+    STR     R1, [r0, #28]   @ set GPIO 16 to HIGH (28) -> LED turns OFF
+    MOV     R2, #0x3F0000   @ initialize wait start amount
+
+/* Wait until R2 == 0 */
+WAIT2$:
+    SUB     R2, #1
+    CMP     R2, #0
+    BNE     WAIT2$
+
+    B       LOOP$
