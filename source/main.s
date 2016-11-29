13f7b28257ffed97e6b760e9eabb971d00f647a9
evanotero <oteroev@bc.edu>
2016-11-28 20:00:32 -0500

    Complete OK01

--- source/main.s
+++ source/main.s
@@ -6,18 +6,18 @@
  * An operating system that turns on the OK LED
  */
 
-.section .init              // directive to linker to put this code first
-.globl _start               // directive to toolchain to tell where entry point is
+.section .init              @ directive to linker to put this code first
+.globl _start               @ directive to toolchain to tell where entry point is
 _start:
-    ldr     r0, =0x20200000 // address of GPIO controller
+    ldr     r0, =0x20200000 @ address of GPIO controller
 
-    mov     r1, #1          // r1 = 1
-    lsl     r1, #18         // 6th set of 3bits -> left shift 6 * 3 = 18 -> r1 = 1000000000000000000
-    str     r1, [r0, #4]    // enable 16th GPIO pin for output
+    mov     r1, #1          @ r1 = 1
+    lsl     r1, #18         @ 6th set of 3bits -> left shift 6 * 3 = 18 -> r1 = 1000000000000000000
+    str     r1, [r0, #4]    @ enable GPIO 16 pin for output
 
-    mov     r1, #1          // r1 = 1
-    mov     r1, #16         // left shift 16 -> r1 = 10000000000000000
-    str     r1, [r0, #40]   //
+    mov     r1, #1          @ r1 = 1
+    mov     r1, #16         @ left shift 16 -> r1 = 10000000000000000
+    str     r1, [r0, #40]   @ set GPIO 16 to low, causing the LED to turn on
 
 loop$:
-    b       loop$           // give Pi task to do forever, so it will not crash
+    b       loop$           @ give Pi task to do forever, so it will not crash
