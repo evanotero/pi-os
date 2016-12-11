bc2b878c4a65c5970bc2b56c8a63d4e12c7d3b89
evanotero <oteroev@bc.edu>
2016-12-10 21:18:45 -0500

    Update to use GPIO47
    On the RPi Zero, GPIO47 is the ACT LED.

--- source/main.s
+++ source/main.s
@@ -3,7 +3,13 @@
  * Author:  Evan Otero
  *
  * ARMv6
- * An operating system that turns the OK LED on and off repeatedly.
+ * An operating system that turns the ACT LED on and off repeatedly.
+ *
+ * IMPORTANT NOTES:
+ * - Pi 0 inherits increased pin count of newer Pi's.
+ * - GPIO47 is now the ACT LED.
+ * - Use GPFSEL4 bit 21 to enable GPIO47.
+ * - Need bit 15 for GPSET1 and GPCLR0.
  */
 
 .section .init              @ directive to linker to put this code first
@@ -13,15 +19,15 @@ _start:
     LDR     R0, =0x20200000 @ address of GPIO controller
 
     MOV     R1, #1
-    LSL     R1, #18         @ 6th set of 3 bits
-    STR     R1, [r0, #4]    @ enable GPIO 16 pin for output
+    LSL     R1, #21         @ 7th set of 3 bits -> GPFSEL4 bit
+    STR     R1, [r0, #16]   @ enable GPIO47 for output
 
     MOV     R1, #1
-    MOV     R1, #16         @ put a 1 in 16th bit of message
+    LSL     R1, #15         @ put a 1 in 15th bit of message
 
 /* Turn LED on and off frepeatedly */
 LOOP$:
-    STR     R1, [r0, #40]   @ set GPIO 16 to LOW (40) -> LED turns ON
+    STR     R1, [r0, #44]   @ set GPIO47 to HIGH (44) -> LED turns ON
     MOV     R2, #0x3F0000   @ initialize wait start amount
 
 /* Wait until R2 == 0 */
@@ -30,7 +36,7 @@ WAIT1$:
     CMP     R2, #0
     BNE     WAIT1$
 
-    STR     R1, [r0, #28]   @ set GPIO 16 to HIGH (28) -> LED turns OFF
+    STR     R1, [r0, #32]   @ set GPIO47 to LOW (32) -> LED turns OFF
     MOV     R2, #0x3F0000   @ initialize wait start amount
 
 /* Wait until R2 == 0 */
