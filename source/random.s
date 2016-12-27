6db34473c781c26c5a004f44b9b2c2b162fa40b9
evanotero <oteroev@bc.edu>
2016-12-27 00:17:17 -0600

    Add Random function

--- source/random.s
+++ source/random.s
@@ -8,3 +8,22 @@
  * - a = 61184; b = 1; c = 73
  */
 
+/*
+    Function with an input of the last number it generated, and an 
+    output of the next number in a pseduo random number sequence.
+    u32 Random(u32 lastValue);
+ */
+.globl Random
+Random:
+    xnm         .req R0
+    a           .req R1
+
+    MOV         a, #0xEF00
+    MUL         a, xnm
+    MUL         a, xnm
+    ADD         a, xnm
+    .unreq      xnm
+    ADD         R0, a, #73
+
+    .unreq      a
+    MOV         PC, LR
