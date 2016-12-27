d37c39cb4bb6df25b57a23e74b6347f991a300fe
evanotero <oteroev@bc.edu>
2016-12-26 22:58:36 -0600

    Init drawing.s

---
+++ source/drawing.s
@@ -0,0 +1,9 @@
+/**
+ * File:    drawing.s
+ * Author:  Evan Otero
+ *
+ * Contains code to do with drawing shapes to the screen.
+ *
+ * IMPORTANT NOTES:
+ * - Address of pixel = frameBufferAddress + (x + y * width) * pixel size
+ */
