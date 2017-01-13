048856fd497f4114621de6f3fe6d9dc198a263f8
evanotero <oteroev@bc.edu>
2017-01-12 18:30:04 -0700

    Fix to print cmdline

--- source/main.s
+++ source/main.s
@@ -49,61 +49,19 @@ ERROR$:
 NOERROR$:
     BL          SetGraphicsAddress
 
-    lastRandom  .req R7
-    lastX       .req R8
-    lastY       .req R9
-    color       .req R10
-    MOV         lastRandom, #0
-    MOV         lastX, #0
-    MOV         lastY, #0
-    MOV         color, #0
+    @ Find the cmdline tag
+    MOV         R0, #9
+    BL          FindTag
 
-    x           .req R5
-    y           .req R6
+    @ Draw our command line
+    LDR         R1, [R0]
+    LSL         R1, #2
+    SUB         R1, #8              @ strnig length
+    ADD         R0, #8
 
-    @ Make lines (loop forever)
-RENDER$:
-    @ Generate next x-coordinate
-    MOV         R0, lastRandom
-    BL          Random
-    MOV         x, R0
+    MOV         R2, #0              @ x-coordinate
+    MOV         R3, #0              @ y-coordinate
+    BL          DrawString
 
-    @ Generate next y-coordinate
-    BL          Random
-    MOV         y, R0
-
-    @ Update last random number
-    MOV         lastRandom, y
-
-    @ Set line color
-    MOV         R0, color
-    BL          SetForeColor
-    ADD         color, #1
-    LSL         color, #16
-    LSR         color, #16      @ set to 0 if above 0xFFFF
-
-    @ Convet x and y to be between 0 and 1023
-    LSR         R2, x, #22
-    LSR         R3, y, #22
-
-    @ Check that y is on screen
-    CMP         R3, #768
-    BHS         RENDER$    
-
-    @ Update last x, y
-    MOV         R0, lastX
-    MOV         R1, lastY
-    MOV         lastX, R2
-    MOV         lastY, R3
-
-    @ Draw line from last x, y
-    BL          DrawLine
-
-    B           RENDER$
-
-    .unreq      x
-    .unreq      y
-    .unreq      lastRandom
-    .unreq      lastX
-    .unreq      lastY
-    .unreq      color
+LOOP$:
+    b LOOP$
